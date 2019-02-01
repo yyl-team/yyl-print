8a0ff20dcda6c613418240a4b356f349ddf5b9d3
jackNEss <jackness1208@qq.com>
2019-02-01 16:16:30 +0800

    * [ADD] 新增 `print.fn.strWrap()` 方法 * [ADD] 新增 `print.log.info()` 对 error 对象， object 对象的输出支持

--- test/run.js
+++ test/run.js
@@ -1,21 +1,4 @@
 const print = require('../index.js');
 // const chalk = require('chalk');
 
-print.log.init({
-  maxSize: 8,
-  type: {
-    concat: {name: 'Concat', color: 'cyan', bgColor: 'bgBlue'}
-  }
-});
-
-print.log.setLogLevel(1);
-
-print.log.warn('aaa');
-print.log.info('111');
-print.log.warn('bbb');
-print.log.info('222');
-print.log.info('333');
-print.log.success('1');
-
-print.log.success('2');
-print.log.success('3');
+console.log(print.fn.strWrap('01234567890123456789', 10, 2))
